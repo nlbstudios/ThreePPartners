57479bf9941a25384ca23427d9f620948bb698bb
NLBSWebDev <webdesign@nlbstudios.com>
2017-03-11 11:51:02 -0500

    Saturday updates

--- vendor/jquery/newjquery.js
+++ vendor/jquery/newjquery.js
@@ -1,14 +1,68 @@
+//The purpose of this jquery document is to add the hiding functionality to the portions of text the user may want to view more of upon a button click
+
+//This is to initially hide the information to be later shown upon button clicks
 $("#welcome-more").hide();
+$("#add-bio").hide();
+$("#add-bio2").hide();
+$("#add-bio3").hide();
+
+//first bio button
 
 $(document).ready(function(){
+  $("#morebio").click(function(){
+    $("#add-bio").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#morebio").click(function(){
+    $("#morebio").fadeToggle(0);
+  });
+});
+
+//second bio button function
+
+$(document).ready(function(){
+  $("#morebio2").click(function(){
+    $("#add-bio2").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#morebio2").click(function(){
+    $("#morebio2").fadeToggle(0);
+  });
+});
+
+//third bio button function
+
+$(document).ready(function(){
+  $("#morebio3").click(function(){
+    $("#add-bio3").fadeToggle(500);
+  });
+});
+
+$(document).ready(function(){
+  $("#morebio3").click(function(){
+    $("#morebio3").fadeToggle(0);
+  });
+});
+///
+$(document).ready(function(){
   $("#morewelcome").click(function(){
-    $("#welcome-more").fadeToggle(1000);
+    $("#welcome-more").fadeToggle(500);
   });
 });
 
 $(document).ready(function(){
   $("#morewelcome").click(function(){
-    $(".welcome-content").animate({"font-size":"16px", "line-height":"16px"})
+    $("#morewelcome").fadeToggle(0);
+  });
+});
+
+$(document).ready(function(){
+  $("#morewelcome").click(function(){
+    $(".welcome-content").animate({"font-size":"18px", "line-height":"25px"}, 0)
   })
 })
 
